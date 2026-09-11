21d8d71b9c078faf6e748abbb1425d7f053da404
Yosef <mercytv932@gmail.com>
2026-09-11 12:26:15 -0400

    login done

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,8 +1,22 @@
+import { Link } from "react-router-dom";
+import { useAuth } from "../contexts/AuthContext";
+
 function NavBar() {
+  const { isAuthenticated, logout } = useAuth();
   return (
-    <div>
-      <h1>Navbar</h1>
-    </div>
+    <nav>
+      <Link to="/">Home</Link>
+      <Link to="/blog">Blog</Link>
+
+      {isAuthenticated ? (
+        <>
+          <Link to="/admin">Admin</Link>
+          <button onClick={logout}>Log Out</button>
+        </>
+      ) : (
+        <Link to="/login">Log In</Link>
+      )}
+    </nav>
   );
 }
 
