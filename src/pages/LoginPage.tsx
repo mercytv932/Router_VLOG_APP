51f8a5ea89cb0662e1eef682904818006050eb4e
Yosef <mercytv932@gmail.com>
2026-09-11 12:01:44 -0400

    App done, Auth and login done pedning

---
+++ src/pages/LoginPage.tsx
@@ -0,0 +1,13 @@
+
+
+function LoginPage() {
+  const handleClick = () => {};
+  return (
+    <div>
+      <h2>Login Page</h2>
+      <button onClick={handleClick}>Login</button>
+    </div>
+  );
+}
+
+export default LoginPage;
