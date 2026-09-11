0e33e37972eb4fa8031a67ba24d09a72fc35176f
Yosef <mercytv932@gmail.com>
2026-09-11 11:14:13 -0400

    blogPage function done

--- src/pages/BlogPage.tsx
+++ src/pages/BlogPage.tsx
@@ -1,7 +1,16 @@
+import { blogs } from "../blogData/data";
+import { Link } from "react-router-dom";
 function BlogPage() {
   return (
     <div>
       <h2>Blog</h2>
+      <ul>
+        {blogs.map((blog) => (
+          <li key={blog.id}>
+            <Link to={`/blog/${blog.slug}`}>{blog.title}</Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
