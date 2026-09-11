057961990ceabd19f48e5623574ff3c845dfdf3c
Yosef <mercytv932@gmail.com>
2026-09-11 11:22:16 -0400

    blogPostPage done

---
+++ src/pages/BlogPostPage.tsx
@@ -0,0 +1,19 @@
+import { blogs } from "../blogData/data";
+import { useParams } from "react-router-dom";
+
+function BlogPostPage() {
+  const { slug } = useParams();
+  const blog = blogs.find((blog) => blog.slug === slug);
+  if (!blog) {
+    return <h2>Post not found</h2>;
+  }
+
+  return (
+    <div>
+      <h2>{blog.title}</h2>
+      <p>{blog.content}</p>
+    </div>
+  );
+}
+
+export default BlogPostPage;
